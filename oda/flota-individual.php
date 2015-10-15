64cc1663cc65f2e9fe247f40bef52fbfd5f67f6e
JUAN <juancvfenix@gmail.com>
2015-10-15 14:38:42 -0500

    añadiendo más recursos de imágenes para oda

---
+++ oda/flota-individual.php
@@ -0,0 +1,194 @@
+<?php $lugar_actual = "index"; ?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+
+    <head>
+
+        <title>Inicio | Grupo Omnibus de Alba</title>
+        <?php include './head.php'; ?>
+
+        <style>
+            .intro-container h1{
+                letter-spacing: 1px;
+            }
+            .intro-container {
+                width: 90%;
+                margin-top: 7%;
+            }
+            .mg-
+        </style>
+
+    </head>
+    <body class="html front not-logged-in no-sidebars page-node page-node- page-node-65 page-type-page-builder spb-enabled i18n-es"
+          id="bigWrapper" data-spy="scroll" data-target=".navbar-default" data-offset="100">
+        <div id="page-wrapper">
+            <div id="page">
+                <div id="top"></div>
+                <?php include './header.php'; ?>
+
+                <div class="region region-content">
+                    <div id="spb-main-wrapper"><!-- #spb-main-wrapper -->
+                        <div id="md-pagebuilder" class="md-pagebuilder">
+                            <div class="md-panel-primary">
+                                <div id="mdpb-live-content">
+                                    <div class="md-section-wrapper desktop">
+                                        <div id="header-banner" class="md-section spb-section-1">
+                                            <div class="container-fluid">
+                                                <div class="row spb-row-11">
+                                                    <div class="spb-column spb-column-111 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                                                        <div class="content">
+                                                            <div id="block-phoenix-config-phoenix-banner"
+                                                                 class="block block-phoenix-config">
+
+                                                                <div class="container  animaper anim ">
+
+
+                                                                </div>
+                                                                <div>
+                                                                    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+                                                                    <!--/* Intro  */-->
+                                                                    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+                                                                    <section id="intro">
+                                                                        <div class="container valign">
+                                                                            <div class="row intro-container text-center">
+                                                                                <div class="col-md-12 text-center">
+                                                                                    <h1>TECNOLOGÍA <span class="text-theme">ATENCIÓN</span> COMODIDAD</h1>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </section>
+                                                                    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+                                                                    <!--/* END Intro  */-->
+                                                                    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+                                                                </div>
+
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div id="about" class="md-section spb-section-4 white" >
+                                            <div class="container">
+                                                <div class="row ">
+                                                    <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12 sectionIntro">
+                                                        <div class="sublayout-column">
+                                                            <div class="mdpb-subrow sub-row-4211">
+                                                                <div class="spb-column spb-column-42111 col-xs-12 col-lg-8 col-md-8 col-sm-10 col-md-offset-2 text-center">
+                                                                    <div class="spb-custom-phoenix-section-header">
+                                                                        <h2 class=""
+                                                                            data-scroll-reveal="enter top move 10px over 1s after 0.2s"
+                                                                            style="color : rgb(40, 40, 40); width: 100%; text-align: center;">
+                                                                            AUTOBUSES <b class="text-theme">INTEGRALES</b>
+                                                                        </h2>
+                                                                    </div>
+                                                                    <div class="spb-custom-phoenix-paragraph">
+                                                                        <hr class="text-theme">
+                                                                    </div>
+                                                                    <div class="spb-custom-phoenix-paragraph">
+                                                                        <p>
+                                                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                                                        </p>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div id="text-separator" class="md-section spb-section-2">
+                                            <div class="container-fluid ours-history">
+                                                <div class="col-md-10 col-md-offset-1 recuadros-flota">
+                                                    <div class="col-md-3" data-scroll-reveal="enter left move 10px over 1s after 0.2s">
+                                                        <h3>Boxer</h3>
+                                                        <img src="http://lorempixel.com/250/250/transport">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                    </div>
+                                                    
+                                                    <div class="col-md-3" data-scroll-reveal="enter left move 10px over 1s after 0.2s">
+                                                        <h3>Boxer</h3>
+                                                        <img src="http://lorempixel.com/250/250/transport">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                    </div>
+                                                    
+                                                    <div class="col-md-3" data-scroll-reveal="enter left move 10px over 1s after 0.2s">
+                                                        <h3>Boxer</h3>
+                                                        <img src="http://lorempixel.com/250/250/transport">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                    </div>
+                                                    
+                                                    <div class="col-md-3" data-scroll-reveal="enter left move 10px over 1s after 0.2s">
+                                                        <h3>Boxer</h3>
+                                                        <img src="http://lorempixel.com/250/250/transport">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                    </div>
+                                                    
+                                                </div>
+                                                <div class="col-md-10 col-md-offset-1 recuadros-flota">
+                                                    <div class="col-md-3" data-scroll-reveal="enter left move 10px over 1s after 0.2s">
+                                                        <h3>Boxer</h3>
+                                                        <img src="http://lorempixel.com/250/250/transport">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                    </div>
+                                                    
+                                                    <div class="col-md-3" data-scroll-reveal="enter left move 10px over 1s after 0.2s">
+                                                        <h3>Boxer</h3>
+                                                        <img src="http://lorempixel.com/250/250/transport">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                    </div>
+                                                    
+                                                    <div class="col-md-3" data-scroll-reveal="enter left move 10px over 1s after 0.2s">
+                                                        <h3>Boxer</h3>
+                                                        <img src="http://lorempixel.com/250/250/transport">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                    </div>
+                                                    
+                                                    <div class="col-md-3" data-scroll-reveal="enter left move 10px over 1s after 0.2s">
+                                                        <h3>Boxer</h3>
+                                                        <img src="http://lorempixel.com/250/250/transport">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                    </div>
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div id="parallax">
+                                            <div class="row parallax-window text-center" data-parallax="scroll" data-image-src="img/parallax.jpg" >
+                                                <h2 class="text-white" data-scroll-reveal="enter up move 10px over 1s after 0.2s">Somos una empresa definida en valores</h2>
+                                                <br><br>
+                                                <a href="#" class="btn-info-parallax" ><b>Servicios</b></a>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.md-primary-panel -->
+                        </div>
+                        <!-- /.md-pagebuilder -->
+                    </div>
+                    <!-- /#spb-main-wrapper -->
+                </div>
+                <?php include './footer.php'; ?>
+            </div>
+        </div>
+        <?php include './scripts.php'; ?>
+    </body>
+</html>
+<script>
+    $.backstretch([
+        "img/bg-03.jpg"
+    ], {duration: 3000, fade: 750});
+</script>
